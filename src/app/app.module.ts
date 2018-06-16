cb862e3e3049e2f1e6d47b6839d2b94a95c19bce
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2018-06-16 12:54:01 +0700

    Added Home, Post, Archive and Search pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
+import { PostPageComponent } from './views/post-page/post-page.component';
+import { ArchivePageComponent } from './views/archive-page/archive-page.component';
+import { SearchPageComponent } from './views/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    PostPageComponent,
+    ArchivePageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/views/archive-page/archive-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArchivePageComponent } from './archive-page.component';
+
+describe('ArchivePageComponent', () => {
+  let component: ArchivePageComponent;
+  let fixture: ComponentFixture<ArchivePageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ArchivePageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArchivePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
