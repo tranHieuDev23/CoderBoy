3b70182222b7ae6892f4beaf8e11fa7402d16618
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2018-06-17 10:46:57 +0700

    Modified top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import globalConfig from './configs/global-config.json'
-import { ButterService } from "./controllers/butterCMS/butter.service";
 
 @Component({
   selector: 'app-root',
@@ -8,20 +7,7 @@ import { ButterService } from "./controllers/butterCMS/butter.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = globalConfig.BUTTERCMS_API_TOKEN
-  public posts: any[]
-
+  
   ngOnInit() {
-    console.log(this.title);
-
-    ButterService.post.list({
-      page: 1,
-      page_size: 10
-    })
-    .then((res) => {
-      console.log('Content from ButterCMS')
-      console.log(res.data)
-      this.posts = res.data.data
-    })
   }
 }
--- src/app/views/components/top-bar/top-bar.component.ts
+++ src/app/views/components/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../../models/category';
+import { ButterService } from '../../../controllers/butterCMS/butter.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
+  private categories: Category[];
+
   constructor() { }
 
   ngOnInit() {
+    ButterService.category.list()
+      .then((res) => {
+        this.categories = res.data.data;
+      }, (res) => {
+        console.log(res.data);
+      })
   }
-
 }
