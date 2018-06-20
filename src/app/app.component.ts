2dc60d761561ae073bbf9fa86f14b189a3c3f3b0
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2018-06-21 00:00:26 +0700

    Added loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone, Renderer2 } from '@angular/core';
 import globalConfig from './configs/global-config.json'
+import { Router, RouterEvent, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,38 @@ import globalConfig from './configs/global-config.json'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  ngOnInit() {
+  @ViewChild('loading') loading: ElementRef;
+
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    private renderer: Renderer2
+  ) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event)
+    })
+  }
+
+  showSpinner() {
+    this.ngZone.runOutsideAngular(() => {
+      this.renderer.removeClass(this.loading.nativeElement, 'hide')
+    })
+  }
+
+  hideSpinner() {
+    this.ngZone.runOutsideAngular(() => {
+      this.renderer.addClass(this.loading.nativeElement, 'hide')
+    })
+  }
+
+  navigationInterceptor(event: RouterEvent) {
+    if (event instanceof NavigationStart)
+      this.showSpinner()
+    if (event instanceof NavigationEnd)
+      this.hideSpinner()
+    if (event instanceof NavigationError)
+      this.hideSpinner()
+    if (event instanceof NavigationCancel)
+      this.hideSpinner()
   }
 }
