0a39e13a229bad70b5bf01cdef262524ab2ca69b
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2018-06-21 21:08:09 +0700

    Top bar now becomes transparent only on specific route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import globalConfig from './configs/global-config.json'
+import { TopBarComponent } from './views/components/top-bar/top-bar.component';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import globalConfig from './configs/global-config.json'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(TopBarComponent) topBar: TopBarComponent;
+
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.handleRoutingEvents(event)
+    })
+  }
+
+  routeAllowsTransparentTopbar(url: string): boolean {
+    let postRegExp = new RegExp('(\/post\/)[a-zA-Z0-9_-]+')
+    return url == '/home' || postRegExp.test(url)
+  }
+
+  handleRoutingEvents(event: RouterEvent): void {
+    if (event instanceof NavigationEnd) {
+      if (this.routeAllowsTransparentTopbar(event.urlAfterRedirects))
+        this.topBar.enableTransparentTopbar();
+      else
+        this.topBar.disableTransparentTopbar();
+    }
+  }
 }
