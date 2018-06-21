627722d498df301675b117befbc155fe52f2084a
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2018-06-21 10:17:30 +0700

    Added prev-next section

--- src/app/views/pages/post-page/post-page.component.ts
+++ src/app/views/pages/post-page/post-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { Post } from '../../../models/post';
 import { ButterService } from '../../../controllers/butterCMS/butter.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { LoadingScreenComponent } from '../../components/loading-screen/loading-screen.component';
 
@@ -14,17 +14,30 @@ import { LoadingScreenComponent } from '../../components/loading-screen/loading-
 export class PostPageComponent implements OnInit {
   @ViewChild(LoadingScreenComponent) loadingScreen: LoadingScreenComponent;
   private post: Post;
+  private prevPost: Post;
+  private nextPost: Post;
 
   constructor(
     private router: Router,
-    private titleService: Title
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((res) =>{
+      this.initView()
+    })
+  }
+
+  initView() {
+    window.scrollTo(0, 0)
+    this.loadingScreen.showSpinner()
     let slug = this.router.url.split('/')[2]
     ButterService.post.retrieve(slug)
       .then((res) => {
         this.post = res.data.data
+        this.prevPost = res.data.meta.previous_post;
+        this.nextPost = res.data.meta.next_post;
         this.titleService.setTitle(this.post.title)
         this.loadingScreen.hideSpinner()
       }, (res) => {
