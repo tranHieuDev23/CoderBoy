81c483259e6592156a571c3dff74bc097292dc31
Tran Minh Hieu <tranhieu23.code@gmail.com>
2018-12-29 19:46:10 +0700

    Optimize the client

--- src/app/views/pages/archive-page/archive-page.component.ts
+++ src/app/views/pages/archive-page/archive-page.component.ts
@@ -13,144 +13,137 @@ const KEY_DATA = makeStateKey('KEY_DATA')
 const KEY_STATUS = makeStateKey('KEY_STATUS')
 
 @Component({
-  selector: 'app-archive-page',
-  templateUrl: './archive-page.component.html',
-  styleUrls: ['./archive-page.component.scss']
+    selector: 'app-archive-page',
+    templateUrl: './archive-page.component.html',
+    styleUrls: ['./archive-page.component.scss']
 })
 export class ArchivePageComponent extends SSRComponent {
-  @ViewChild(LoadingScreenComponent) loadingScreen: LoadingScreenComponent
-  public author: Author
-  public posts: Post[]
-  public title: string
-  public currentPage: number
-  public lastPage: number
-  public baseUrl: string
+    @ViewChild(LoadingScreenComponent) loadingScreen: LoadingScreenComponent
+    public author: Author
+    public posts: Post[]
+    public title: string
+    public currentPage: number
+    public lastPage: number
+    public baseUrl: string
 
-  constructor(
-    activatedRoute: ActivatedRoute,
-    @Inject(PLATFORM_ID) platformId: Object,
-    @Optional() @Inject(RESPONSE) response: any,
-    transferState: TransferState,
-    private router: Router,
-    private titleService: Title,
-    private metaService: Meta
-  ) {
-    super(activatedRoute, platformId, response, transferState)
-  }
-
-  onBrowserInit(params: any) {
-    this.loadingScreen.showSpinner()
-    let status = this.transferState.get(KEY_STATUS, null)
-    this.transferState.set(KEY_STATUS, null)
-    if (status == '404') {
-      this.router.navigateByUrl('/404', {
-        skipLocationChange: true,
-        replaceUrl: false
-      })
-      return
+    constructor(
+        activatedRoute: ActivatedRoute,
+        @Inject(PLATFORM_ID) platformId: Object,
+        @Optional() @Inject(RESPONSE) response: any,
+        transferState: TransferState,
+        private router: Router,
+        private titleService: Title,
+        private metaService: Meta
+    ) {
+        super(activatedRoute, platformId, response, transferState)
     }
 
-    let data = this.transferState.get(KEY_DATA, null)
-    this.transferState.set(KEY_DATA, null)
-    if (data != null) {
-      this.initView(data.resultMeta, data.resultPosts, data.type, data.slug, data.currentPage)
-      return
-    }
+    onBrowserInit(params: any) {
+        this.loadingScreen.showSpinner()
+        let status = this.transferState.get(KEY_STATUS, null)
+        this.transferState.set(KEY_STATUS, null)
+        if (status == '404') {
+            this.router.navigateByUrl('/404', {
+                skipLocationChange: true,
+                replaceUrl: false
+            })
+            return
+        }
 
-    let type = params['type']
-    if (type != 'category' && type != 'tag' && type != 'author') {
-      this.router.navigateByUrl('/404', {
-        skipLocationChange: true,
-        replaceUrl: false
-      })
-      return
-    }
-    let slug = params['slug']
+        let data = this.transferState.get(KEY_DATA, null)
+        this.transferState.set(KEY_DATA, null)
+        if (data != null) {
+            this.initView(data.resultMeta, data.resultPosts, data.type, data.slug, data.currentPage)
+            return
+        }
 
-    ButterService[type].retrieve(slug)
-    .then((resultMeta) => {
-      let currentPage = params['page'] != null? +params['page'] : 1
-      const REQUEST_PARAMS: any = {
-        page: currentPage,
-        page_size: GlobalConfig.ARCHIVE_PAGE_SIZE
-      }
-      
-      if (type == 'category')
-        REQUEST_PARAMS.category_slug = slug
-      if (type == 'tag')
-        REQUEST_PARAMS.tag_slug = slug
-      if (type == 'author')
-        REQUEST_PARAMS.author_slug = slug
+        let type = params['type']
+        if (type != 'category' && type != 'tag' && type != 'author') {
+            this.router.navigateByUrl('/404', {
+                skipLocationChange: true,
+                replaceUrl: false
+            })
+            return
+        }
+        let slug = params['slug']
+        let currentPage = params['page'] != null ? +params['page'] : 1
+        const REQUEST_PARAMS: any = {
+            page: currentPage,
+            page_size: GlobalConfig.ARCHIVE_PAGE_SIZE
+        }
 
-      ButterService.post.list(REQUEST_PARAMS)
-        .then((resultPosts) => {
-          this.initView(resultMeta, resultPosts, type, slug, currentPage)
-          window.scrollTo(0, 0)
-        }, () => {
-          this.router.navigateByUrl('/404', {
-            skipLocationChange: true,
-            replaceUrl: false
-          })
+        if (type == 'category')
+            REQUEST_PARAMS.category_slug = slug
+        if (type == 'tag')
+            REQUEST_PARAMS.tag_slug = slug
+        if (type == 'author')
+            REQUEST_PARAMS.author_slug = slug
+
+        let metaPromise = ButterService[type].retrieve(slug)
+        let postsPromise = ButterService.post.list(REQUEST_PARAMS)
+
+        Promise.all([metaPromise, postsPromise]).then((result) => {
+            this.initView(result[0], result[1], type, slug, currentPage)
+            window.scrollTo(0, 0)
+        }, (error) => {
+            this.router.navigateByUrl('/404', {
+                skipLocationChange: true,
+                replaceUrl: false
+            })
         })
-    }, () => {
-      this.router.navigateByUrl('/404', {
-        skipLocationChange: true,
-        replaceUrl: false
-      })
-    })
-  }
-
-  onServerInit(params: Params) {
-    if (this.response.locals.status == '404') {
-      this.router.navigateByUrl('/404', {
-        skipLocationChange: true,
-        replaceUrl: false
-      })
-      this.transferState.set(KEY_STATUS, '404')
-      this.response.status(404)
-      return
     }
-    
-    let data = this.response.locals.data
-    this.initView(data.resultMeta, data.resultPosts, data.type, data.slug, data.currentPage)
-    this.transferState.set(KEY_DATA, data)
-  }
 
-  initView(resultMeta: any, resultPosts: any, type: string, slug: string, currentPage: number): void {
-    this.title = this.generateTitle(type, resultMeta.data.data)
-    if (type == 'author') {
-      this.author = resultMeta.data.data
-      this.titleService.setTitle(`${resultMeta.data.data.last_name} ${resultMeta.data.data.first_name}`)
-    } else {
-      this.titleService.setTitle(resultMeta.data.data.name)
+    onServerInit(params: Params) {
+        if (this.response.locals.status == '404') {
+            this.router.navigateByUrl('/404', {
+                skipLocationChange: true,
+                replaceUrl: false
+            })
+            this.transferState.set(KEY_STATUS, '404')
+            this.response.status(404)
+            return
+        }
+
+        let data = this.response.locals.data
+        this.initView(data.resultMeta, data.resultPosts, data.type, data.slug, data.currentPage)
+        this.transferState.set(KEY_DATA, data)
     }
-    this.posts = resultPosts.data.data
-    this.currentPage = currentPage
-    this.lastPage = Math.floor(
-      (resultPosts.data.meta.count + GlobalConfig.ARCHIVE_PAGE_SIZE - 1) / GlobalConfig.ARCHIVE_PAGE_SIZE
-    )
-    this.baseUrl = `/archive/${type}/${slug}`
-    this.loadingScreen.hideSpinner()
-    this.addSEOMetaTags()
-  }
 
-  generateTitle(type: string, data: any): string {
-    if (type == 'category')
-      return `Bài viết thuộc chủ đề ${data.name}`
-    if (type == 'tag')
-      return `Bài viết gắn tag ${data.name}`
-    if (type == 'author')
-      return `Bài viết của tác giả ${data.last_name} ${data.first_name}`
-  }
+    initView(resultMeta: any, resultPosts: any, type: string, slug: string, currentPage: number): void {
+        this.title = this.generateTitle(type, resultMeta.data.data)
+        if (type == 'author') {
+            this.author = resultMeta.data.data
+            this.titleService.setTitle(`${resultMeta.data.data.last_name} ${resultMeta.data.data.first_name}`)
+        } else {
+            this.titleService.setTitle(resultMeta.data.data.name)
+        }
+        this.posts = resultPosts.data.data
+        this.currentPage = currentPage
+        this.lastPage = Math.floor(
+            (resultPosts.data.meta.count + GlobalConfig.ARCHIVE_PAGE_SIZE - 1) / GlobalConfig.ARCHIVE_PAGE_SIZE
+        )
+        this.baseUrl = `/archive/${type}/${slug}`
+        this.loadingScreen.hideSpinner()
+        this.addSEOMetaTags()
+    }
 
-  addSEOMetaTags(): void {
-    this.metaService.addTags([
-      {name: 'description', content: `Trang ${this.currentPage} trên ${this.lastPage}`},
-      {property: 'og:url', content: this.router.url},
-      {property: 'og:title', content: this.title},
-      {property: 'og:description', content: `Trang ${this.currentPage} trên ${this.lastPage}`},
-      {property: 'og:image', content: GlobalConfig.BLOG_FEATURE_IMAGE_URL},
-      {property: 'og:type', content: 'website'},
-    ])
-  }
+    generateTitle(type: string, data: any): string {
+        if (type == 'category')
+            return `Bài viết thuộc chủ đề ${data.name}`
+        if (type == 'tag')
+            return `Bài viết gắn tag ${data.name}`
+        if (type == 'author')
+            return `Bài viết của tác giả ${data.last_name} ${data.first_name}`
+    }
+
+    addSEOMetaTags(): void {
+        this.metaService.addTags([
+            { name: 'description', content: `Trang ${this.currentPage} trên ${this.lastPage}` },
+            { property: 'og:url', content: this.router.url },
+            { property: 'og:title', content: this.title },
+            { property: 'og:description', content: `Trang ${this.currentPage} trên ${this.lastPage}` },
+            { property: 'og:image', content: GlobalConfig.BLOG_FEATURE_IMAGE_URL },
+            { property: 'og:type', content: 'website' },
+        ])
+    }
 }
--- src/app/views/pages/home-page/home-page.component.ts
+++ src/app/views/pages/home-page/home-page.component.ts
@@ -13,80 +13,76 @@ import { CarouselComponent } from '../../components/carousel/carousel.component'
 const KEY_DATA = makeStateKey('KEY_DATA')
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent extends SSRComponent {
-  @ViewChild(LoadingScreenComponent) loadingScreen: LoadingScreenComponent
-  @ViewChild(CarouselComponent) carousel: CarouselComponent
-  public posts: Post[]
-  public categories: Category[]
+    @ViewChild(LoadingScreenComponent) loadingScreen: LoadingScreenComponent
+    @ViewChild(CarouselComponent) carousel: CarouselComponent
+    public posts: Post[]
+    public categories: Category[]
 
-  constructor(
-    activatedRoute: ActivatedRoute,
-    @Inject(PLATFORM_ID) platformId: Object,
-    @Optional() @Inject(RESPONSE) response: any,
-    transferState: TransferState,
-    private router: Router,
-    private titleService: Title,
-    private metaService: Meta
-  ) {
-    super(activatedRoute, platformId, response, transferState)
-  }
-
-  onBrowserInit(params: Params) {
-    this.loadingScreen.showSpinner()
-    let data = this.transferState.get(KEY_DATA, null)
-    this.transferState.set(KEY_DATA, null)
-
-    if (data) {
-      this.initView(data.resultCategories, data.resultPosts)
-      window.scrollTo(0, 0)
-      return
+    constructor(
+        activatedRoute: ActivatedRoute,
+        @Inject(PLATFORM_ID) platformId: Object,
+        @Optional() @Inject(RESPONSE) response: any,
+        transferState: TransferState,
+        private router: Router,
+        private titleService: Title,
+        private metaService: Meta
+    ) {
+        super(activatedRoute, platformId, response, transferState)
     }
-    
-    ButterService.category.list()
-      .then((resultCategories) => {
-        ButterService.post.list({
-          page: 1,
-          page_size: GlobalConfig.CAROUSEL_PAGES,
-          exclude_body: true
-        }).then((resultPosts) => {
-          this.initView(resultCategories, resultPosts)
-          window.scrollTo(0, 0)
-        }, () => {
-          this.router.navigateByUrl('/404', {
-            skipLocationChange: true,
-            replaceUrl: false
-          })
-        })
-      }, () => {
-        this.router.navigateByUrl('/404', {
-          skipLocationChange: true,
-          replaceUrl: false
-        })
-      })
-  }
 
-  onServerInit(params: Params) {
-    let data = this.response.locals.data
-    this.initView(data.resultCategories, data.resultPosts)
-    this.transferState.set(KEY_DATA, data)
-  }
+    onBrowserInit(params: Params) {
+        this.loadingScreen.showSpinner()
+        let data = this.transferState.get(KEY_DATA, null)
+        this.transferState.set(KEY_DATA, null)
 
-  initView(resultCategories: any, resultPosts: any): void {
-    this.categories = resultCategories.data.data
-    this.posts = resultPosts.data.data
-    this.titleService.setTitle(GlobalConfig.BLOG_TITLE)
-    this.loadingScreen.hideSpinner()
-    this.metaService.addTags([
-      {name: 'description', content: GlobalConfig.BLOG_DESCRIPTION},
-      {property: 'og:url', content: this.router.url},
-      {property: 'og:title', content: GlobalConfig.BLOG_TITLE},
-      {property: 'og:description', content: GlobalConfig.BLOG_DESCRIPTION},
-      {property: 'og:image', content: GlobalConfig.BLOG_FEATURE_IMAGE_URL},
-      {property: 'og:type', content: 'website'},
-    ])
-  } 
+        if (data) {
+            this.initView(data.resultCategories, data.resultPosts)
+            window.scrollTo(0, 0)
+            return
+        }
+
+        let categoryPromise = ButterService.category.list()
+        let postsPromise = ButterService.post.list({
+            page: 1,
+            page_size: GlobalConfig.CAROUSEL_PAGES,
+            exclude_body: true
+        })
+
+        Promise.all([categoryPromise, postsPromise])
+            .then((result) => {
+                this.initView(result[0], result[1])
+                window.scrollTo(0, 0)
+            }, (error) => {
+                this.router.navigateByUrl('/404', {
+                    skipLocationChange: true,
+                    replaceUrl: false
+                })
+            })
+    }
+
+    onServerInit(params: Params) {
+        let data = this.response.locals.data
+        this.initView(data.resultCategories, data.resultPosts)
+        this.transferState.set(KEY_DATA, data)
+    }
+
+    initView(resultCategories: any, resultPosts: any): void {
+        this.categories = resultCategories.data.data
+        this.posts = resultPosts.data.data
+        this.titleService.setTitle(GlobalConfig.BLOG_TITLE)
+        this.loadingScreen.hideSpinner()
+        this.metaService.addTags([
+            { name: 'description', content: GlobalConfig.BLOG_DESCRIPTION },
+            { property: 'og:url', content: this.router.url },
+            { property: 'og:title', content: GlobalConfig.BLOG_TITLE },
+            { property: 'og:description', content: GlobalConfig.BLOG_DESCRIPTION },
+            { property: 'og:image', content: GlobalConfig.BLOG_FEATURE_IMAGE_URL },
+            { property: 'og:type', content: 'website' },
+        ])
+    }
 }
